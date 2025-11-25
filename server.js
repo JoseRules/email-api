9ec6581f24ea6c2210ca0b82a63b3873f220d17a
Jose Flores <jose.fgonzalez@alumnos.udg.mx>
2025-11-24 18:02:45 -0600

    adding gdl glow connection

--- server.js
+++ server.js
@@ -5,13 +5,27 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors());
-const {EMAIL_KEY, EMAIL_DEST} = process.env;
+const {EMAIL_KEY, EMAIL_DEST, SEC_EMAIL_DEST} = process.env;
 const resend = new Resend(EMAIL_KEY);
 
 app.use(express.json());
 
 app.post('/data', async (req, res) => {
-  const {name, email, message, authenticated} = req.body;
+  const {name, email, message, authenticated, phone, notes, cartItems, total, order_source} = req.body;
+  let candleOrderTemplate = '';
+  if(order_source === 'gdlglow'){
+    candleOrderTemplate = `
+    <p>name: <strong>${name}</strong></p>
+    <p>email: <strong>${email}</strong></p>
+    <p>phone: <strong>${phone}</strong></p>
+    <p>notes: <strong>${notes}</strong></p>
+    <p>cartItems: <strong>${cartItems.map(item => {
+      const optionsText = item.selectedOptions ? Object.entries(item.selectedOptions).map(([key, value]) => `${key}: ${value}`).join(', ') : '';
+      return `<li>${item.name} - ${item.quantity}${optionsText ? ` (${optionsText})` : ''}</li>`;
+    }).join('')}</strong></p>
+    <p>total: <strong>${total}</strong></p>
+  `;
+  }
   const unaauthenticatedTemplate = "<strong>someone is viewing your portfolio</strong>";
   const authenticatedTemplate = `
     <p>name: <strong>${name}</strong></p>
@@ -19,10 +33,10 @@ app.post('/data', async (req, res) => {
     <p>message: <strong>${message}</strong></p>
   `;
   const { data, error } = await resend.emails.send({
-    from: "Portfolio <onboarding@resend.dev>",
-    to: [EMAIL_DEST],
+    from: order_source === 'gdlglow' ? "GDG Glow <onboarding@resend.dev>" : "Portfolio <onboarding@resend.dev>",
+    to: order_source === 'gdlglow' ? [EMAIL_DEST, SEC_EMAIL_DEST] : [EMAIL_DEST],
     subject: authenticated ? "Important message!!!" : "View alert!",
-    html: authenticated ? authenticatedTemplate : unaauthenticatedTemplate,
+    html: authenticated ? authenticatedTemplate : (order_source === 'gdlglow' ? candleOrderTemplate : unaauthenticatedTemplate),
   });
 
   if (error) {
